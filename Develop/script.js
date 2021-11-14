38ac627f8f73002a61a751e490c99216d8566786
dmknapp2385 <dmknapp2385@gmail.com>
2021-11-14 11:25:09 -0700

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,53 @@
-// Assignment code here
+// Array of prompts with answers
+var prompts = [
+  {
+    criteria: 'Upper case'
+  },
+  {
+    criteria: 'Lower case'
+  },
+  {
+    criteria: 'Number'
+  },
+  {
+    criteria: 'Special Character'
+  }
+];
 
+// function for loop to get array responses
+var generateCriteria = function () {
+  for (i = 0; 1 < 4; i++) {
+    var yesOrNo = window.prompt("Would you like your password to contain" + prompts[i].criteria + "? Respond with 'yes' or 'no'.");
+    yesOrNo = yesOrNo.toLowerCase();
+
+    if (!yesOrNo || yesOrNo === 'no') {
+      prompts[i].value = false;
+    }
+    else {
+      prompts[i].value = true;
+    }
+  }
+  var checkCriteria = function () {
+    for (i = 0; i < 4; i++) {
+      counter = ''
+      criteriaChecked = []
+      if (prompts[i].value === true) {
+        counter += 1;
+        criteriaChecked.push(i);
+      }
+    if (counter = 0) {
+      window.alert("You must check at aleast one criteria. Please try again.");
+      generateCriteria();
+    }
+    else{
+      return criteriaChecked;
+    }
+  } 
+}
+
+var generatePassword = function () {
+  generateCriteria();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +58,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writepassword); 
