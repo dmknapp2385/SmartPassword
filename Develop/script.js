2365bba33021f0762eb2c0f075597b01b4fe00dc
dmknapp2385 <dmknapp2385@gmail.com>
2021-11-14 12:00:31 -0700

    updated brackets and random number function

--- Develop/script.js
+++ Develop/script.js
@@ -2,51 +2,60 @@
 var prompts = [
   {
     criteria: 'Upper case'
+
   },
   {
     criteria: 'Lower case'
   },
   {
-    criteria: 'Number'
+    criteria: 'Special Character'
   },
   {
-    criteria: 'Special Character'
+    criteria: 'Number',
+    number:"",
+    randomNumber: function () {
+       this. number = Math.floor(Math.random() * 10);
+    } 
   }
 ];
 
-// function for loop to get array responses
+// function for loop to get prompt responses
 var generateCriteria = function () {
   for (i = 0; 1 < 4; i++) {
     var yesOrNo = window.prompt("Would you like your password to contain" + prompts[i].criteria + "? Respond with 'yes' or 'no'.");
     yesOrNo = yesOrNo.toLowerCase();
 
+    var pickedPrompt = prompts[i]
+
     if (!yesOrNo || yesOrNo === 'no') {
-      prompts[i].value = false;
+        pickedPrompt.value= false;
     }
     else {
-      prompts[i].value = true;
+        pickedPrompt.value= true;
     }
   }
+  // for loop to check that at least one prompt was a 'yes'
   var checkCriteria = function () {
-    for (i = 0; i < 4; i++) {
-      counter = ''
-      criteriaChecked = []
-      if (prompts[i].value === true) {
-        counter += 1;
-        criteriaChecked.push(i);
+      for (i = 0; i < 4; i++) {
+        counter = ''
+        if (prompts[i].value === true) {
+          counter += 1;
+        }
       }
-    if (counter = 0) {
+      if (counter = 0) {
       window.alert("You must check at aleast one criteria. Please try again.");
       generateCriteria();
-    }
-    else{
-      return criteriaChecked;
-    }
-  } 
+    } 
+  }
 }
 
 var generatePassword = function () {
   generateCriteria();
+  // prompt to ask for length of password
+  var passwordLength = window.prompt("How many characters would you like your password to be? Choose a number between 8 and 123.")
+  
+  passwordLength = parseInt(passwordLength);
+
 }
 
 // Get references to the #generate element
@@ -60,4 +69,4 @@ function writePassword() {
   passwordText.value = password;
 }
 
-generateBtn.addEventListener("click", writepassword); 
+generateBtn.addEventListener("click", writepassword()); 
